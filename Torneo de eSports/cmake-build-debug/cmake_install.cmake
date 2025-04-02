b5cf7d7042dec12a6d82b2a2d4b138c546be9edc
Alexbp <alexbp200612@gmail.com>
2025-04-02 10:24:38 -0500

    Adjunto Encuesta

--- Torneo de eSports/cmake-build-debug/cmake_install.cmake
+++ Torneo de eSports/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/alexb/OneDrive/Desktop/Proyecto poo/untitled
+# Install script for directory: C:/Users/alexb/OneDrive/Desktop/Proyecto poo/Torneo Videojuego/9-practica-contenedores-toreno-Alexbp12/Torneo de eSports
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/alexb/OneDrive/Desktop/Proyecto poo/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/alexb/OneDrive/Desktop/Proyecto poo/Torneo Videojuego/9-practica-contenedores-toreno-Alexbp12/Torneo de eSports/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
